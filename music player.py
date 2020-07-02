65a1dac23157cfdd94384cc435cfd7a5554dd30a
mohammad <59312286+mohammad-pixel@users.noreply.github.com>
2020-07-02 11:51:23 +0530

    add playlist class
    class playlist ke database PLAYLIST ijad
    mikonad va mitavanad be in data base
    esm ahang dar library ra ezafe konad va
    kam konad va ahang haye on ra namayesh dahad

--- music player.py
+++ music player.py
@@ -1,5 +1,5 @@
-import os
-#class library baraye gharar dadan e ahang ha dar library proje in kar ba copy kardan ahang anjam mishavad
+import os, sqlite3, datetime
+#class library baraye gharar dadan e music ha dar library in kar ba copy kardan ahang anjam mishavad
 class library:
     #dastor __init__ adrese music ra daryaft mikonad
     def __init__(self, MusicAdrress):
@@ -17,3 +17,52 @@ class library:
     #ba methode Delete music dar file e library hazf mishavad
     def Delete(FileName):
         os.remove(os.getcwd()+"\\library"+"\\"+FileName)
+#class play list ke esm ahang ha ra dar PLAYLIST ke yek data base hast gharar midahad
+class play_list:
+    #method esm ahang ke ezafe mishavad ra migirad
+    def __init__(self, MusicName):
+        self.MusicName=MusicName
+        self.__Add()
+    #method ahang av zamane gharar dadan ra be PLAYLIST add mikonad
+    def __Add(self):
+        try:
+            Connection = sqlite3.connect(os.getcwd() + "\\PLAYLIST.db")
+            Curser = Connection.cursor()
+            Curser.execute("""CREATE TABLE IF NOT EXISTS PLAYLIST(
+                 music char PRIMARY KEY,
+                 TimeAdd char
+                 );""")
+            Query= """INSERT INTO PLAYLIST
+                    (music, TimeAdd)
+                    VALUES
+                    (?,?)"""
+            Curser.execute(Query, (self.MusicName, datetime.datetime.now()))
+            Connection.commit()
+            Curser.close()
+            Connection.close()
+        except:
+            print("music already added")
+    #method list mojod dar PLAYLIST ra bar migardanad
+    def Show(self):
+        try:
+            Connection = sqlite3.connect(os.getcwd() + "\\PLAYLIST.db")
+            Curser = Connection.cursor()
+            Queray="select * from PLAYLIST;"
+            Curser.execute(Queray)
+            x=Curser.fetchall()
+            Curser.close()
+            Connection.close()
+            return x
+        except:
+            print(f"eror: {sqlite3.Error}")
+    #method ahang ra az PLAYLIST hazf mikonad
+    def Delete(self, MusicName):
+        try:
+            Connection = sqlite3.connect(os.getcwd() + "\\PLAYLIST.db")
+            Curser = Connection.cursor()
+            Curser.execute('DELETE FROM PLAYLIST WHERE music=?', (MusicName,))
+            Connection.commit()
+            Curser.close()
+            Connection.close()
+        except:
+            print(sqlite3.Error)
