e162cea1e3c47257491b8433625388ae969f65cc
mohammad <59312286+mohammad-pixel@users.noreply.github.com>
2020-07-11 15:27:02 +0530

    ijade class CreateToolTip
    برای نمایش اطلاعات ویدجت

--- music player.py
+++ music player.py
@@ -484,6 +484,62 @@ class click_volume:
         #tanzim e seda
         pygame.mixer.music.set_volume(Volume / 100)
 
+#method baraye ijade Tooltip baraye widget ha(copy shode az internet :)    )
+class CreateToolTip:
+    """
+    create a tooltip for a given widget
+    """
+
+    def __init__(self, widget, text):
+        self.waittime = 50  # miliseconds
+        self.wraplength = 100  # pixels
+        self.widget = widget
+        self.text = text
+        self.widget.bind("<Enter>", self.enter)
+        self.widget.bind("<Leave>", self.leave)
+        self.widget.bind("<ButtonPress>", self.leave)
+        self.id = None
+        self.tw = None
+        self.lib = True
+
+    def enter(self, event=None):
+        self.schedule()
+
+    def leave(self, event=None):
+        self.unschedule()
+        self.hidetip()
+
+    def schedule(self):
+        self.unschedule()
+        self.id = self.widget.after(self.waittime, self.showtip)
+
+    def unschedule(self):
+        id = self.id
+        self.id = None
+        if id:
+            self.widget.after_cancel(id)
+
+    def showtip(self, event=None):
+        x = y = 0
+        x, y, cx, cy = self.widget.bbox("insert")
+        x += self.widget.winfo_rootx() + 35
+        y += self.widget.winfo_rooty() + 30
+        # creates a toplevel window
+        self.tw = tkinter.Toplevel(self.widget)
+        # Leaves only the label and removes the app window
+        self.tw.wm_overrideredirect(True)
+        self.tw.wm_geometry("+%d+%d" % (x, y))
+        label = tkinter.Label(self.tw, text=self.text, justify='left',
+                              background="white", relief='solid', borderwidth=1,
+                              wraplength=self.wraplength)
+        label.pack(ipadx=1)
+
+    def hidetip(self):
+        tw = self.tw
+        self.tw = None
+        if tw:
+            tw.destroy()
+
 #class baraye left menu barname(entekhabe playlist, library, hzf va ezafe kardan be library ya playlist)
 class menu:
 
